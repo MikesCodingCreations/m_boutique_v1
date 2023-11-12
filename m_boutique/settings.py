ffe235f11df965b7d6f1043b6fe953c7658679c5
Mikes Coding Creations <mikescodingcreations@gmail.com>
2023-11-12 11:58:21 +0000

    CSS Link updated

--- m_boutique/settings.py
+++ m_boutique/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mikescoding-mboutiquev1-d30lpp5mg59.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-mikescoding-mboutiquev1-j52w885r4t8.ws-eu106.gitpod.io']
 
 
 # Application definition
